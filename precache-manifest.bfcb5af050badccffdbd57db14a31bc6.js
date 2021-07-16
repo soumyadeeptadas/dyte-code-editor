5d498a85e10d58dba7d28e0c10bd2a85e09a86a5
unknown <sdpta1998@gmail.com>
2021-07-17 00:42:01 +0530

    Updates

---
+++ precache-manifest.bfcb5af050badccffdbd57db14a31bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f29b4b6ca50404b3da3e",
+    "url": "/dyte-code-editor/static/css/main.f5bad870.chunk.css"
+  },
+  {
+    "revision": "f29b4b6ca50404b3da3e",
+    "url": "/dyte-code-editor/static/js/main.f29b4b6c.chunk.js"
+  },
+  {
+    "revision": "05684ffe29a7e64f729a",
+    "url": "/dyte-code-editor/static/css/1.ce9c962c.chunk.css"
+  },
+  {
+    "revision": "05684ffe29a7e64f729a",
+    "url": "/dyte-code-editor/static/js/1.05684ffe.chunk.js"
+  },
+  {
+    "revision": "f2fb1faea4b13972ec7c",
+    "url": "/dyte-code-editor/static/js/runtime~main.f2fb1fae.js"
+  },
+  {
+    "revision": "71f02cc4b87f965143e76c4e7691ab5c",
+    "url": "/dyte-code-editor/index.html"
+  }
+];
